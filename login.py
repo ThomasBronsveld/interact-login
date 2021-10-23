335f077cae78a347c2f421fbe656973ea0d18c67
Kamil Maliszewski <Kamil.Maliszewski@hva.nl>
2021-10-23 16:58:12 +0200

    chenged the way how the key is given to a more readable and expandable way with usage of variable arguments, also added comments to code

--- login.py
+++ login.py
@@ -3,23 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from sqlalchemy import MetaData, create_engine
 from config import DB_LOGIN, DB_PASSWORD, DB_SERVER, DB_NAME
-import sys
 from cryptography.fernet import Fernet
+import argparse
 
+
+#gets runtime arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-K", "--key", help="encryption key")
+args = parser.parse_args()
+
+#unciphers text (credentials) by using key from runtime arguments and returns them as plain text
 def decrypt_credentials(text):
-  key = sys.argv[1]
+  #check if key was given during runtime
+  if not args.key:
+    return "No key"
+  key = args.key
   cipher_suite = Fernet(key)
   unciphered = cipher_suite.decrypt(text)
   plain_text = bytes(unciphered).decode("utf-8")
-  return plain_text 
+  return plain_text
 
+#decrypts sql credentials
 db_login = decrypt_credentials(DB_LOGIN)
 db_pass = decrypt_credentials(DB_PASSWORD)
 db_server = decrypt_credentials(DB_SERVER)
 db_name = decrypt_credentials(DB_NAME)
-
-app = Flask(__name__)
 db_url = 'mysql://{}:{}@{}/{}'.format(db_login, db_pass, db_server, db_name)
+
+#flask configuration
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
@@ -39,11 +51,12 @@ def testdb():
   except Exception as e:
     return e
 
-
+#verifies if login given matches with any db user
 @app.route('/user/verify/login', methods=['POST'])
 def verify_login():
   try:
     data = request.get_json()
+    #check if user login was given as his username or his email
     if '@' in data['login']:
       user = User.query.filter_by(email=data['login'], password=data['password']).first()
     else:
